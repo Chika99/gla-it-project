fcb9676a7b82f9b7eb8e0837505c2a2c43d142ef
Quincy <huadeng00@163.com>
2022-03-07 11:16:19 +0000

    feat: rebuild login and register

--- static/js/register.js
+++ static/js/register.js
@@ -1,89 +1,126 @@
-$(function () {
+function check_username() {
+  const len = $("#username").val().length;
+  if ((len > 0 && len < 5) || len > 20) {
+    $("#username").next().html("Username should between 5-20 characters!");
+    $("#username").next().show();
+  } else {
+    $("#username").next().hide();
+  }
+}
+function check_password() {
+  const len = $("#password").val().length;
+  if ((len > 0 && len < 6) || len > 20) {
+    $("#password").next().html("Password should between 6-20 characters!");
+    $("#password").next().show();
+  } else {
+    console.log("in");
+    $("#password").next().hide();
+  }
+}
+function check_conform_password() {
+  if ($("#password").val() !== $("#confirm_password").val()) {
+    $("#confirm_password")
+      .next()
+      .html(
+        "Password doesn't match. Please check that you've entered the same password twice"
+      );
+    $("#confirm_password").next().show();
+  } else {
+    $("#confirm_password").next().hide();
+  }
+}
+// $(function () {
+//   var error_name = false;
+//   var error_password = false;
+//   var error_check_password = false;
+//   var error_email = false;
+//   var error_check = false;
 
-    var error_name = false;
-    var error_password = false;
-    var error_check_password = false;
-    var error_email = false;
-    var error_check = false;
+//   $("#user_name").blur(function () {
+//     check_user_name();
+//   });
 
+//   $("#pwd").blur(function () {
+//     check_pwd();
+//   });
 
-    $('#user_name').blur(function () {
-        check_user_name();
-    });
+//   $("#cpwd").blur(function () {
+//     check_cpwd();
+//   });
 
-    $('#pwd').blur(function () {
-        check_pwd();
-    });
+//   $("#email").blur(function () {
+//     check_email();
+//   });
 
-    $('#cpwd').blur(function () {
-        check_cpwd();
-    });
+//   function check_user_name() {
+//     var len = $("#user_name").val().length;
+//     if (len < 5 || len > 20) {
+//       $("#user_name")
+//         .next()
+//         .html("Please enter a username between 5-20 characters!");
+//       $("#user_name").next().show();
+//       error_name = true;
+//     } else {
+//       $("#user_name").next().hide();
+//       error_name = false;
+//     }
+//   }
 
-    $('#email').blur(function () {
-        check_email();
-    });
+//   function check_pwd() {
+//     var len = $("#pwd").val().length;
+//     if (len < 6 || len > 20) {
+//       $("#pwd").next().html("Please enter a password between 6-20 characters!");
+//       $("#pwd").next().show();
+//       error_password = true;
+//     } else {
+//       $("#pwd").next().hide();
+//       error_password = false;
+//     }
+//   }
 
-    function check_user_name() {
-        var len = $('#user_name').val().length;
-        if (len < 5 || len > 20) {
-            $('#user_name').next().html('Please enter a username between 5-20 characters!')
-            $('#user_name').next().show();
-            error_name = true;
-        } else {
-            $('#user_name').next().hide();
-            error_name = false;
-        }
-    }
+//   function check_cpwd() {
+//     var pass = $("#pwd").val();
+//     var cpass = $("#cpwd").val();
 
-    function check_pwd() {
-        var len = $('#pwd').val().length;
-        if (len < 6 || len > 20) {
-            $('#pwd').next().html('Please enter a password between 6-20 characters!')
-            $('#pwd').next().show();
-            error_password = true;
-        } else {
-            $('#pwd').next().hide();
-            error_password = false;
-        }
-    }
+//     if (pass != cpass) {
+//       $("#cpwd").next().html("The two entered passwords do not match!");
+//       $("#cpwd").next().show();
+//       error_check_password = true;
+//     } else {
+//       $("#cpwd").next().hide();
+//       error_check_password = false;
+//     }
+//   }
 
-    function check_cpwd() {
-        var pass = $('#pwd').val();
-        var cpass = $('#cpwd').val();
+//   function check_email() {
+//     var re = /^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/;
 
-        if (pass != cpass) {
-            $('#cpwd').next().html('The two entered passwords do not match!')
-            $('#cpwd').next().show();
-            error_check_password = true;
-        } else {
-            $('#cpwd').next().hide();
-            error_check_password = false;
-        }
-    }
+//     if (re.test($("#email").val())) {
+//       $("#email").next().hide();
+//       error_email = false;
+//     } else {
+//       $("#email").next().html("The email is not correct!");
+//       $("#email").next().show();
+//       error_check_password = true;
+//     }
+//   }
 
-    function check_email() {
-        var re = /^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$/;
+//   $("#reg_form").submit(function () {
+//     check_user_name();
+//     check_pwd();
+//     check_cpwd();
+//     check_email();
 
-        if (re.test($('#email').val())) {
-            $('#email').next().hide();
-            error_email = false;
-        } else {
-            $('#email').next().html('The email is not correct!')
-            $('#email').next().show();
-            error_check_password = true;
-        }
-    }
-
-    $('#reg_form').submit(function () {
-        check_user_name();
-        check_pwd();
-        check_cpwd();
-        check_email();
-
-        if (error_name == false && error_password == false && error_check_password == false && error_email == false && error_check == false) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-})
+//     if (
+//       error_name == false &&
+//       error_password == false &&
+//       error_check_password == false &&
+//       error_email == false &&
+//       error_check == false
+//     ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   });
+// });
