6dfffa08fee00489a1a4bc1391774c5c869dcf8c
Quincy <huadeng00@163.com>
2022-03-16 18:52:01 +0000

    feat: change profile page except avatar

---
+++ static/js/user_modify.js
@@ -0,0 +1,21 @@
+function handle_change_avatar(files) {
+    const imgs = document.getElementsByClassName("uploaded_images_div");
+    const length = imgs.length;
+    if(length == 1){
+      const img = document.getElementsByClassName("uploaded_images_div")[0];
+      img.parentNode.removeChild(img);
+    }
+    if(files.length==1){
+      const windowURL = window.URL || window.webkitURL;
+      const dataURL = windowURL.createObjectURL(files[0]);
+      const div = document.createElement("div");
+      div.className = "uploaded_images_div";
+      const image = document.createElement("img");
+      image.src = dataURL;
+      image.className = "uploaded_images";
+      div.appendChild(image);
+      document
+        .getElementById("images_out")
+        .insertBefore(div, document.getElementById("input_label"));
+    }
+  }
