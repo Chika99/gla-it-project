ea83622f783882fa62de83edb710dfe301f3a27a
Dongyao Wang <2612859w@student.gla.ac.uk>
2022-03-17 16:11:42 +0000

    models test for Bid

--- app/tests_model.py
+++ app/tests_model.py
@@ -2,16 +2,16 @@
 import datetime
 from django.test import TestCase
 
-from app.models import Order, Tag, User
+from app.models import Bid, Order, Tag, User
 
-
+#test for models.User
 class UserTest(TestCase):
 
     #create default database for test
     def setUp(self) -> None:
         User.objects.create(username="test0", tel=654321, balance=0, credit_level=10, address="Glasgow")
 
-    #test for create a user and find this user in database
+    #test for creating a user and find this user in database
     def test1_CreateUser(self):
         User.objects.create(username="test1", tel=123456, balance=100, credit_level=0, address="London")
         a = User.objects.get(username="test1")
@@ -21,7 +21,7 @@ class UserTest(TestCase):
         self.assertEqual(a.credit_level, 0)
         self.assertEqual(a.address, "London")
 
-    #test for delete the default user which is created in setUp()
+    #test for deleting the default user which is created in setUp()
     def test2_DeleteUser(self):
         a = User.objects.get(username="test0")
         a.delete()
@@ -29,7 +29,7 @@ class UserTest(TestCase):
         c = len(b)
         self.assertEqual(c, 0)
 
-    #test for update a user and find this user in database
+    #test for updating a user and find this user in database
     def test3_UpdateUser(self):
         a = User.objects.get(username="test0")
         a.username = "update"
@@ -45,7 +45,7 @@ class UserTest(TestCase):
         self.assertEqual(b.credit_level, 100)
         self.assertEqual(b.address, "West")
 
-    #test for find user
+    #test for finding user
     def test4_FindUser(self):
         User.objects.create(username="test1", tel=123456, balance=100, credit_level=0, address="London")
         a = User.objects.filter(username__contains="test")
@@ -58,6 +58,7 @@ class UserTest(TestCase):
         #none can be found
         self.assertEqual(len(c), 0)
 
+#test for models.Order
 class OrderTest(TestCase):
 
     #create default datebase for test
@@ -66,7 +67,7 @@ class OrderTest(TestCase):
         buyer0 = User.objects.create(username="buyer0")
         Order.objects.create(id=0, seller=seller0, buyer=buyer0, title="test0", description="new", start_price=200, end_time=datetime.datetime(2022, 3, 21, 0, 0))
 
-    #test for create an order and find it
+    #test for creating an order and find it
     def test5_CreateOrder(self):
         seller1 = User.objects.create(username="seller1")
         buyer1 = User.objects.create(username="buyer1")
@@ -78,7 +79,7 @@ class OrderTest(TestCase):
         self.assertEqual(a.description, "null")
         self.assertEqual(a.end_time, datetime.datetime(2022, 3, 20, 0, 0))
 
-    #test for delete an order
+    #test for deleting an order
     def test6_DeleteOrder(self):
         a = Order.objects.get(id=0)
         a.delete()
@@ -86,7 +87,7 @@ class OrderTest(TestCase):
         c = len(b)
         self.assertEqual(c, 0)
 
-    #test for update an order and check its information correct or not   
+    #test for updating an order and check its information correct or not   
     def test7_UpdateOrder(self):
         seller1 = User.objects.create(username="seller1")
         buyer1 = User.objects.create(username="buyer1")
@@ -106,7 +107,7 @@ class OrderTest(TestCase):
         self.assertEqual(b.description, "fresh")
         self.assertEqual(b.end_time, datetime.datetime(2023, 3, 20, 0, 0))
 
-    #test for find order
+    #test for finding order
     def test8_FindOrder(self):
         seller1 = User.objects.create(username="seller1")
         buyer1 = User.objects.create(username="buyer1")
@@ -122,6 +123,7 @@ class OrderTest(TestCase):
         #none can be found
         self.assertEqual(len(c), 0)
 
+#test for models.Tag
 class TagTest(TestCase):
 
     #set up default database for test
@@ -158,6 +160,70 @@ class TagTest(TestCase):
         Tag.objects.create(id=1, name="tag1")
         a = Tag.objects.filter(name__contains="t")
         self.assertEqual(len(a), 2)
-        b = User.objects.filter(id__contains=123)
+        b = Tag.objects.filter(id__contains=123)
         self.assertEqual(len(b), 0)
+
+#test for models.Bid
+class BidTest(TestCase):
+
+    #create default database for testing
+    def setUp(self):
+        seller0 = User.objects.create(username="seller0")
+        buyer0 = User.objects.create(username="buyer0")
+        Order0 = Order.objects.create(id=0, seller=seller0, buyer=buyer0, title="test0", description="new", start_price=200, end_time=datetime.datetime(2022, 3, 21, 0, 0))    
+        Bid.objects.create(id=0, order=Order0, user=buyer0, price=300)
+    
+    #test for creating bid
+    def test13_CreateBid(self):
+        seller1 = User.objects.create(username="seller1")
+        buyer1 = User.objects.create(username="buyer1")
+        Order1 = Order.objects.create(id=1, seller=seller1, buyer=buyer1, title="test1", description="old", start_price=100, end_time=datetime.datetime(2023, 3, 21, 0, 0))    
+        Bid.objects.create(id=1, order=Order1, user=buyer1, price=500)
+        a = Bid.objects.get(id=1)
+        self.assertEqual(a.id, 1)
+        self.assertEqual(a.order, Order1)
+        self.assertEqual(a.user, buyer1)
+        self.assertEqual(a.price, 500)
+
+    #test for deleting bid
+    def test14_DeleteBid(self):
+        a = Bid.objects.get(id=0)
+        a.delete()
+        b = Bid.objects.filter(id=0)
+        self.assertEqual(len(b), 0)
+
+    #test for updating bid
+    def test15_UpdateBid(self):
+        #create some argument for setting to current bid 
+        seller1 = User.objects.create(username="seller1")
+        buyer1 = User.objects.create(username="buyer1")
+        Order1 = Order.objects.create(id=1, seller=seller1, buyer=buyer1, title="test1", description="old", start_price=100, end_time=datetime.datetime(2023, 3, 21, 0, 0))    
+        # get bid whose id is 0
+        a = Bid.objects.get(id=0)
+        a.id = 3
+        a.order = Order1
+        a.user = seller1
+        a.price = 1000
+        a.save()
+        self.assertEqual(a.id, 3)
+        self.assertEqual(a.order, Order1)
+        self.assertEqual(a.user, seller1)
+        self.assertEqual(a.price, 1000)
+
+    #test for finding bid
+    def test16_FindBid(self):
+        #create an new bid
+        seller1 = User.objects.create(username="seller1")
+        buyer1 = User.objects.create(username="buyer1")
+        Order1 = Order.objects.create(id=1, seller=seller1, buyer=buyer1, title="test1", description="old", start_price=100, end_time=datetime.datetime(2023, 3, 21, 0, 0))    
+        Bid.objects.create(id=1, order=Order1, user=buyer1, price=500)
+        #there are 2 bids in database now
+        a = Bid.objects.filter(price__contains=0)
+        self.assertEqual(len(a), 2)
+        b = Bid.objects.filter(price__contains=500)
+        self.assertEqual(len(b), 1)
+        c = Bid.objects.filter(user=buyer1)
+        self.assertEqual(len(c), 1)
+
+
 
